2544e3d4fdbac75e5efc146b9ade6754f53d50c8
DESKTOP-72N4IHA\MINKYU <minkyu2184@gmail.com>
2024-02-28 15:21:11 +0900

    Footer 생성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import {Routes, Route, useNavigate} from 'react-router-dom';
-import {useState, useEffect} from 'react'
-import Header from './Component/Header'
+import {useState, useEffect} from 'react';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
 import Main from './Pages/Main';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
@@ -139,6 +140,7 @@ function App() {
           )
         }
       />
+
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Login" element={<Login />}></Route>
@@ -167,7 +169,11 @@ function App() {
         <Route path="/Shop/Basket" element={<ShopBasket />} />
         <Route path="/MyPage" element={<MyPage />} />
       </Routes>
-    </div>
+      
+      <Footer/>
+      s
+      </div>
+
   );
 }
 
---
+++ my-app/src/Component/Footer.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './Footer.css';
+
+function Footer() {
+    const [isListVisible, setListVisible] = useState(false);
+
+    const toggleListVisibility = () => {
+        console.log("toggleListVisibility 함수가 호출되었습니다.");
+        setListVisible((prevState) => !prevState);
+        console.log("isListVisible:", isListVisible);
+    };
+
+    return (
+        <footer className="Footer">
+            <div className="FooterLayer1">
+                <span>FooterIsNarrowed</span>
+            </div>
+            <div className="FooterLayer2">
+
+                <div className="FooterContent">
+                    <div className="leftContent">
+                    <h2>빵끗😊</h2>
+                    <p>탄소중립플랫폼</p>
+                    </div>
+                    <address>
+                        "상호명 및 호스팅 서비스 제공 : 빵끗"
+                        <br />
+                        "관리자 : Ezteam2CommunityTeam"
+                        <br />
+                        "인천광역시 남동구 인주대로 593 엔타스빌딩 12층"
+                        <br />
+                        "등록번호 : 000-0000-000"
+                    </address>
+                    <div className="labelList">
+                        <div className="related">
+                            <div>
+                            <button className={`label ${isListVisible ? 'active' : ''}`} onClick={toggleListVisibility}>
+                                    관련 사이트 바로가기 {isListVisible ? '▲' : '▼'}
+                                </button>
+                                <div className={`list ${isListVisible ? 'active' : ''}`}>
+                                    <ul>
+                                    <a onClick={() => window.open("https://me.go.kr/home/web/main.do", "_blank")} title="새창">환경부</a>
+                                    <a onClick={() => window.open("https://www.keci.or.kr/web/main.do", "_blank")} title="새창">한국환경보전원</a>
+                                    <a onClick={() => window.open("https://www.gihoo.or.kr/zerolife", "_blank")} title="새창">탄소중립 실천포털</a>
+                                    <a onClick={() => window.open("https://www.gihoo.or.kr/greencampus/", "_blank")} title="새창">그린캠퍼스</a>
+                                    </ul>
+                                     </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="FooterLayer3">
+                <span>FooterIsNarrowed</span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
