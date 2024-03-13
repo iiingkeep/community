f9d22e025c212e21e3a9e47c9a482354a7b364f1
HUJO733 <wngh1716@gmail.com>
2024-03-13 11:10:57 +0900

    로그인 유효성

--- my-app/src/Pages/Login.js
+++ my-app/src/Pages/Login.js
@@ -1,44 +1,54 @@
-import React, { useState} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import "../Styles/Login.css"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../Styles/Login.css";
 
 //로그인 페이지 상태 변화 함수
 function Login() {
-  const [email, setemail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const [loginStatus,setloginStatus]= useState('');
-  
+  const [loginStatus, setloginStatus] = useState("");
+
+  const IDcheck = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{5,20}$/; // ID 정규표현식
+
   const LoginPageJs = () => {
-  
-    // 로그인 요청 구현
-    axios.post('http://localhost:8000/Login', {
-      email: email,
-      password: password
-    }) //회원 정보 email, password 정보 가져옴
-    .then(response => {
-      console.log('서버 응답:', response);
-      if (response.data.success) {
-        const { userid, username } = response.data.data[0]; // 익스플로우세션
-        const userData={
-          userid: userid,
-          username: username
-        }
-        sessionStorage.setItem('loggedIn', true);
-        sessionStorage.setItem('userData',JSON.stringify(userData) );
-        // Application에 세션스토리지 안에서 정보를 출력한다
-  
-        navigate('/');
-        window.location.reload(); // 페이지 리로드
-      } else {
-        // 로그인 실패 시 처리
-        console.log('로그인 실패 : ', response.data);
-        setloginStatus('로그인 실패 : '+ response.data.message);
-      }
-    })
+    if (!email) {
+      setloginStatus("아이디를 입력하세요.");
+    } else if (!IDcheck.test(email)) {
+      setloginStatus("아이디 형식이 올바르지 않습니다.");
+    } else if (!password) {
+      setloginStatus("비밀번호를 입력하세요.");
+    } else {
+      // 로그인 요청 구현
+      axios
+        .post("http://localhost:8000/login", {
+          email: email,
+          password: password,
+        }) //회원 정보 email, password 정보 가져옴
+        .then((response) => {
+          console.log("서버 응답:", response);
+          if (response.data.success) {
+            const { userid, username } = response.data.data[0]; // 익스플로우세션
+            const userData = {
+              userid: userid,
+              username: username,
+            };
+            sessionStorage.setItem("loggedIn", true);
+            sessionStorage.setItem("userData", JSON.stringify(userData));
+            // Application에 세션스토리지 안에서 정보를 출력한다
+
+            navigate("/");
+            window.location.reload(); // 페이지 리로드
+          } else {
+            // 로그인 실패 시 처리
+            console.log("로그인 실패 : ", response.data);
+            setloginStatus("로그인 실패 : " + response.data.message);
+          }
+        });
+    }
   };
-  
+
   return (
     <div className="login-page">
       <div className="login-box">
