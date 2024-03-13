8248a96bed3cbda5740881dc347c43c8fbd283f1
DESKTOP-R85HIPO\user <smlsk12@naver.com>
2024-03-14 07:49:17 +0900

    마이페이지 css수정_3

--- my-app/src/Component/ImagePopup.js
+++ my-app/src/Component/ImagePopup.js
@@ -1,16 +1,12 @@
 // ImagePopup.js
-
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import '../Styles/ImagePopup.css';
 import Draggable from "./Draggable";
-import { useImage } from "./ImageContext"; // context추가
 
-const ImagePopup = ({ userId, onClose }) => {
+const ImagePopup = ({ userId, onClose, handleProfileImg }) => {
   // 이미지 URL 상태
-  const { imageUrl, setImageUrl } = useImage(); // 수정후*
-  // const [imageUrl, setImageUrl] = useState(""); // 수정전*
-
+  const [imageUrl, setImageUrl] = useState(""); 
   // file input 요소접근
   const fileInputRef = useRef(null); 
 
@@ -29,7 +25,7 @@ const ImagePopup = ({ userId, onClose }) => {
       // 이미지가 존재하는 파일명으로 경로 수정 - *수정코드
       const imageUrl = `http://localhost:8000/public/userimg/${result.data}`;
       setImageUrl(imageUrl);
-      // handleProfileImg(imageUrl);
+      handleProfileImg(imageUrl);
       // 업로드 성공 시 서버에서 받은 이미지 URL을 상태에 설정 - *오류코드
       // const imageUrl = `http://localhost:8000/my/profile/img/${result.data}`;
       
@@ -54,40 +50,27 @@ const ImagePopup = ({ userId, onClose }) => {
       alert('이미지가 선택되지 않았습니다.');
     }
   };
+  
+  // 이미지url을 상태에서 제거하는 함수
+  const handleImageDelete = () => {
+    setImageUrl("");
+    // 로컬스토리지에서 이미지url 제거
+    localStorage.removeItem('storageImg');
+  };
 
-// 이미지를 가져오는 함수 *수정중
-// const handleSaveImage = async () => {
-//   try {
-//     const response = await axios.get(`http://localhost:8000/imgsave/${userId}`);
-//     // 이미지 파일의 경로를 받아옵니다.
-//     const imageUrl = response.data;
-//     console.log(imageUrl);
-//     // 이미지 파일의 경로를 상태에 설정하여 이미지를 표시합니다.
-//     setImageUrl(imageUrl); 
-//   } catch (error) {
-//     console.error('이미지 가져오기 오류:', error);
-//     alert('이미지 가져오기 오류');
-//   }
-// };
+  // 이미지url을 저장하는 함수
+  const handleSaveImage = () => {
+    // 이미지url이 존재할 경우에 저장
+    // key = saveImgUrl / value = imageUrl
+    if (imageUrl) {
+      localStorage.setItem('storageImg', imageUrl);
+      alert('이미지 저장');
+      onClose();
+    } else {
+      alert('이미지 없음');
+    }
+  };
 
-// useEffect(() => {
-//   handleSaveImage();
-// }, [userId]);
-
-
-  // 이미지를 서버에서 삭제
-  // const handleImageDelete = async () => {
-  //   try {
-  //     await axios.delete('http://localhost:8000/imgdelete', {
-  //       data: { imageUrl } // 이미지 URL을 서버에 전달하여 삭제
-  //     });
-  //     setImageUrl("");
-  //     alert('이미지 삭제 완료');
-  //   } catch (error) {
-  //     console.error('이미지 삭제 오류:', error);
-  //     alert('이미지 삭제 오류');
-  //   }
-  // };
 
   // 파일 선택 창 이벤트 버튼
   const handleUploadBtnRef = () => {
@@ -98,15 +81,21 @@ const ImagePopup = ({ userId, onClose }) => {
     }
   };
 
+  useEffect(() => {
+    const savedImageUrl = localStorage.getItem('storageImg');
+    if (savedImageUrl) {
+      setImageUrl(savedImageUrl);
+    }
+  }, []);
 
   return (
-    <div className="Modal">
+    <div className="img-modal">
       <Draggable>
-        <div className="ModalContent">
-          <button onClick={onClose}>X</button>
+        <div>
+          <button className="img-modal__close" onClick={onClose}>X</button>
           <br />
           {/* 업로드 버튼 참조 */}
-          <button className="UploadBtn" onClick={handleUploadBtnRef}>이미지 업로드</button>
+          <button className="img-modal__btn" onClick={handleUploadBtnRef}>이미지 업로드</button>
           {/* 파일 업로드 버튼 */}
           <input
             type="file"
@@ -118,10 +107,10 @@ const ImagePopup = ({ userId, onClose }) => {
           /> 
           <br />
           {imageUrl && ( // 이미지 URL이 존재할 경우 이미지 표시
-              <img className="ProfileImg" src={imageUrl} alt="이미지" />
+              <img className="img-modal__content" src={imageUrl} alt="이미지" />
           )}
-          {/* <button onClick={handleSaveImage}>저장</button> */}
-          {/* <button onClick={handleImageDelete}>삭제</button> */}
+          <button className="img-modal__btn" onClick={handleSaveImage}>저장</button>
+          <button className="img-modal__btn" onClick={handleImageDelete}>삭제</button>
         </div>
       </Draggable>
     </div>
@@ -130,17 +119,19 @@ const ImagePopup = ({ userId, onClose }) => {
 
 export default ImagePopup;
 
-
 // ImagePopup.js
 
-// import { useEffect, useState, useRef } from "react";
+// import { useState, useRef, useEffect } from "react";
 // import axios from "axios";
 // import '../Styles/ImagePopup.css';
 // import Draggable from "./Draggable";
+// import { useImage } from "./ImageContext"; // context추가
 
-// const ImagePopup = ({ onClose, handleProfileImg }) => {
+// const ImagePopup = ({ userId, onClose }) => {
 //   // 이미지 URL 상태
-//   const [imageUrl, setImageUrl] = useState(""); 
+//   const { imageUrl, setImageUrl } = useImage(); // 수정후*
+//   // const [imageUrl, setImageUrl] = useState(""); // 수정전*
+
 //   // file input 요소접근
 //   const fileInputRef = useRef(null); 
 
@@ -149,7 +140,8 @@ export default ImagePopup;
 //     formData.append('img', file);
 
 //     try {
-//       const result = await axios.post('http://localhost:8000/my/profile/img', formData,
+//       // const result = await axios.post('http://localhost:8000/my/profile/img', formData,
+//       const result = await axios.post(`http://localhost:8000/imgupdate/${userId}`, formData,
 //       { headers: { 'Content-Type': 'multipart/form-data' }}
 //       );
 //       console.log('success: 서버응답', result.data);
@@ -158,7 +150,7 @@ export default ImagePopup;
 //       // 이미지가 존재하는 파일명으로 경로 수정 - *수정코드
 //       const imageUrl = `http://localhost:8000/public/userimg/${result.data}`;
 //       setImageUrl(imageUrl);
-//       handleProfileImg(imageUrl);
+//       // handleProfileImg(imageUrl);
 //       // 업로드 성공 시 서버에서 받은 이미지 URL을 상태에 설정 - *오류코드
 //       // const imageUrl = `http://localhost:8000/my/profile/img/${result.data}`;
       
@@ -183,27 +175,40 @@ export default ImagePopup;
 //       alert('이미지가 선택되지 않았습니다.');
 //     }
 //   };
-  
-//   // 이미지url을 상태에서 제거하는 함수
-//   const handleImageDelete = () => {
-//     setImageUrl("");
-//     // 로컬스토리지에서 이미지url 제거
-//     localStorage.removeItem('storageImg');
-//   };
 
-//   // 이미지url을 저장하는 함수
-//   const handleSaveImage = () => {
-//     // 이미지url이 존재할 경우에 저장
-//     // key = saveImgUrl / value = imageUrl
-//     if (imageUrl) {
-//       localStorage.setItem('storageImg', imageUrl);
-//       alert('이미지 저장');
-//       onClose();
-//     } else {
-//       alert('이미지 없음');
-//     }
-//   };
+// // 이미지를 가져오는 함수 *수정중
+// // const handleSaveImage = async () => {
+// //   try {
+// //     const response = await axios.get(`http://localhost:8000/imgsave/${userId}`);
+// //     // 이미지 파일의 경로를 받아옵니다.
+// //     const imageUrl = response.data;
+// //     console.log(imageUrl);
+// //     // 이미지 파일의 경로를 상태에 설정하여 이미지를 표시합니다.
+// //     setImageUrl(imageUrl); 
+// //   } catch (error) {
+// //     console.error('이미지 가져오기 오류:', error);
+// //     alert('이미지 가져오기 오류');
+// //   }
+// // };
 
+// // useEffect(() => {
+// //   handleSaveImage();
+// // }, [userId]);
+
+
+//   // 이미지를 서버에서 삭제
+//   // const handleImageDelete = async () => {
+//   //   try {
+//   //     await axios.delete('http://localhost:8000/imgdelete', {
+//   //       data: { imageUrl } // 이미지 URL을 서버에 전달하여 삭제
+//   //     });
+//   //     setImageUrl("");
+//   //     alert('이미지 삭제 완료');
+//   //   } catch (error) {
+//   //     console.error('이미지 삭제 오류:', error);
+//   //     alert('이미지 삭제 오류');
+//   //   }
+//   // };
 
 //   // 파일 선택 창 이벤트 버튼
 //   const handleUploadBtnRef = () => {
@@ -214,21 +219,15 @@ export default ImagePopup;
 //     }
 //   };
 
-//   useEffect(() => {
-//     const savedImageUrl = localStorage.getItem('storageImg');
-//     if (savedImageUrl) {
-//       setImageUrl(savedImageUrl);
-//     }
-//   }, []);
 
 //   return (
-//     <div className="Modal">
+//     <div className="modal">
 //       <Draggable>
-//         <div className="ModalContent">
+//         <div className="modal-content">
 //           <button onClick={onClose}>X</button>
 //           <br />
 //           {/* 업로드 버튼 참조 */}
-//           <button className="UploadBtn" onClick={handleUploadBtnRef}>이미지 업로드</button>
+//           <button className="UploadBtn" onClick={handleUploadBtnRef}>이미지선택</button>
 //           {/* 파일 업로드 버튼 */}
 //           <input
 //             type="file"
@@ -242,8 +241,8 @@ export default ImagePopup;
 //           {imageUrl && ( // 이미지 URL이 존재할 경우 이미지 표시
 //               <img className="ProfileImg" src={imageUrl} alt="이미지" />
 //           )}
-//           <button onClick={handleSaveImage}>저장</button>
-//           <button onClick={handleImageDelete}>삭제</button>
+//           {/* <button onClick={handleSaveImage}>저장</button> */}
+//           {/* <button onClick={handleImageDelete}>삭제</button> */}
 //         </div>
 //       </Draggable>
 //     </div>
--- my-app/src/Component/ProfileForm.js
+++ my-app/src/Component/ProfileForm.js
@@ -1,123 +1,17 @@
-// ProfileForm.js
-import axios from "axios";
-import { useEffect, useState } from "react";
-import ImagePopup from "./ImagePopup";
-import "../Styles/MyPage.css";
-import { useImage } from "./ImageContext"; // context추가
-
-const ProfileForm = ({userId}) => {
-    // 회원 정보 상태관리
-    const [profileData, setProfileData] = useState([]);
-    // 이미지 URL 상태관리
-    const { imageUrl, setImageUrl } = useImage(); // 수정후*
-    // const [imageUrl, setImageUrl] = useState(""); // 수정전*
-    
-    // 이미지팝업창 클릭이벤트 상태관리
-    const [showPopup, setShowPopup] = useState(false);
-
-    
-
-    // // 이미지 업데이트 핸들러
-    const handleProfileImg = (url) => {
-        setImageUrl(url);
-    };
-    // 이미지팝업창 클릭이벤트
-    const handleImgPopup = () => {
-        setShowPopup(true);
-    };
-
-    useEffect(() => {
-        // 페이지 로드 시 저장된 이미지url 가져와서 상태에 설정
-        const savedImageUrl = localStorage.getItem('storageImg');
-        if (savedImageUrl) {
-            setImageUrl(savedImageUrl);
-        }
-
-        // fetchProfile 함수를 useEffect 내부에서 정의하고 호출
-        const fetchProfile = async () => {
-            try {
-                const response = await axios.get(`http://localhost:8000/my/profile/${userId}`);
-                const userData = response.data[0]; // n번째 데이터
-                setProfileData(userData);
-                // console.log(userData)
-            } catch (error) {
-                console.error('Error: fetching profile data:', error);
-            }
-        };
-
-        // fetchProfile 함수 호출
-        // userId가 변경될 때마다 호출
-        fetchProfile();
-    }, [userId, setImageUrl]); // 오류 해결을 위해 setImageUrl 추가
-
-    return (
-        <div className="profile-form inner" >
-            <div className="my-form__title">
-                <p className="my-form__text">나의 프로필</p>
-            </div>
-
-            <div className="my-form__wrapper">
-                <div className="profile-form__img">
-            {/* 업로드 된 이미지가 없다면 user_img폴더의 이미지를 표시 */}
-            {imageUrl ? ( <img src={imageUrl} alt="userimg" /> ) : ( <img className="profile-form__img" src="/user_img/basic.png" alt="IMG" /> )}
-            <button className="profile__btn" onClick={handleImgPopup}>사진 변경</button>
-            </div>
-
-            <div className="profile-content">
-            {/* 이미지 팝업 */}
-            {/* ImagePopup에 userId 전달 */}
-            {showPopup && <ImagePopup userId={userId} onClose={() => setShowPopup(false)} handleProfileImg={handleProfileImg}/>}
-            
-            <div className="profile-content-wrapper">
-            {/* id식별 조건부 렌더링 */}
-            {profileData.usertype === '2' || profileData.usertype === '3'  
-            ? ( <p>사업자번호 {profileData.businessnumber}</p> ) : null}
-            
-            <div className="profile-name-wrapper">
-            <span className="profile__name">{profileData.username}</span>
-            <span> 님</span>
-            </div>
-            <table class="profile-content__detail">
-    <tr>
-        <td>이메일</td>
-        <td>{profileData.email}</td>
-    </tr>
-    <tr>
-        <td>핸드폰번호</td>
-        <td>{profileData.phonenumber}</td>
-    </tr>
-    <tr>
-        <td>주소</td>
-        <td>{profileData.address}</td>
-    </tr>
-    <tr>
-        <td>상세주소</td>
-        <td>{profileData.detailedaddress}</td>
-    </tr>
-</table>
-
-
-            </div>
-
-            </div>
-            
-        </div>
-        </div>
-    );
-};
-export default ProfileForm;
-
 // // ProfileForm.js
 // import axios from "axios";
 // import { useEffect, useState } from "react";
 // import ImagePopup from "./ImagePopup";
-// import "../Styles/ProfileForm.css"
+// import "../Styles/MyPage.css";
+// import { useImage } from "./ImageContext"; // context추가
 
 // const ProfileForm = ({userId}) => {
 //     // 회원 정보 상태관리
 //     const [profileData, setProfileData] = useState([]);
 //     // 이미지 URL 상태관리
-//     const [imageUrl, setImageUrl] = useState("");
+//     const { imageUrl, setImageUrl } = useImage(); // 수정후*
+//     // const [imageUrl, setImageUrl] = useState(""); // 수정전*
+    
 //     // 이미지팝업창 클릭이벤트 상태관리
 //     const [showPopup, setShowPopup] = useState(false);
 
@@ -152,30 +46,166 @@ export default ProfileForm;
 //         };
 
 //         // fetchProfile 함수 호출
+//         // userId가 변경될 때마다 호출
 //         fetchProfile();
-//     }, [userId]); // userId가 변경될 때마다 호출
+//     }, [userId, setImageUrl]); // 오류 해결을 위해 setImageUrl 추가
 
 //     return (
-//         <div className="profile-form" >
-//             <h2>Profile Form</h2>
-//             {/* 업로드 된 이미지가 없다면 user_img폴더의 이미지를 표시 */}
-//             <div className="profileImg">
-//             {imageUrl ? ( <img src={imageUrl} alt="Profile" /> ) : ( <img src="/user_img/basic.png" alt="DefaultIMG" /> )}
+//         <div className="profile-form inner" >
+//             <div className="my-form__title">
+//                 <p className="my-form__text">나의 프로필</p>
 //             </div>
-//             <br />
-//             {/* 이미지 팝업 */}
-//             <button onClick={handleImgPopup}>사진 변경</button>
-//             {showPopup && <ImagePopup onClose={() => setShowPopup(false)} handleProfileImg={handleProfileImg}/>}
-//             {/* 프로필 정보 */}
-//             {/* id식별 조건부 렌더링 */}
-//             {profileData.usertype === '2' || profileData.usertype === '3'  
-//                 ? ( <p>사업자번호: {profileData.businessnumber}</p> ) : null}
-//             <p>회원이름: {profileData.username}</p>
-//             <p>핸드폰번호: {profileData.phonenumber}</p>
-//             <p>주소: {profileData.address}</p>
-//             <p>상세주소: {profileData.detailedaddress}</p>
-//             <p>이메일: {profileData.email}</p>
+
+//             <div className="my-form__wrapper">
+//                 <div className="profile-form__img">
+//                     {/* 업로드 된 이미지가 없다면 user_img폴더의 이미지를 표시 */}
+//                     {imageUrl ? (<img src={imageUrl} alt="userimg" />) 
+//                     : (<img className="profile-form__img" src="/user_img/basic.png" alt="IMG" />)}
+//                     </div>
+//                     <div className="profile-wrapper__btn">
+//                     <button className="profile__btn" onClick={handleImgPopup}>사진 변경</button>
+//                 </div>
+
+//                 <div className="profile-content">
+//                     {/* 이미지 팝업 */}
+//                     {/* ImagePopup에 userId 전달 */}
+//                     {showPopup && <ImagePopup userId={userId} onClose={() => setShowPopup(false)} handleProfileImg={handleProfileImg} />}
+
+//                     <div className="profile-content-wrapper">
+//                         {/* id식별 조건부 렌더링 */}
+//                         {profileData.usertype === '2' || profileData.usertype === '3'
+//                             ? (<p>사업자번호 {profileData.businessnumber}</p>) : null}
+
+//                         <div className="profile-name-wrapper">
+//                             <span className="profile__name">{profileData.username}</span>
+//                             <span> 님</span>
+//                         </div>
+//                         <table class="profile-content__detail">
+//                             <tr>
+//                                 <td>이메일</td>
+//                                 <td>{profileData.email}</td>
+//                             </tr>
+//                             <tr>
+//                                 <td>휴대전화</td>
+//                                 <td>{profileData.phonenumber}</td>
+//                             </tr>
+//                             <tr>
+//                                 <td>주소</td>
+//                                 <td>{profileData.address}</td>
+//                             </tr>
+//                             <tr>
+//                                 <td>상세주소</td>
+//                                 <td>{profileData.detailedaddress}</td>
+//                             </tr>
+//                         </table>
+//                     </div>
+//                 </div>
+//             </div>
 //         </div>
 //     );
 // };
 // export default ProfileForm;
+
+// ProfileForm.js
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ImagePopup from "./ImagePopup";
+import "../Styles/MyPage.css";
+
+const ProfileForm = ({userId}) => {
+    // 회원 정보 상태관리
+    const [profileData, setProfileData] = useState([]);
+    // 이미지 URL 상태관리
+    const [imageUrl, setImageUrl] = useState("");
+    // 이미지팝업창 클릭이벤트 상태관리
+    const [showPopup, setShowPopup] = useState(false);
+
+    
+
+    // // 이미지 업데이트 핸들러
+    const handleProfileImg = (url) => {
+        setImageUrl(url);
+    };
+    // 이미지팝업창 클릭이벤트
+    const handleImgPopup = () => {
+        setShowPopup(true);
+    };
+
+    useEffect(() => {
+        // 페이지 로드 시 저장된 이미지url 가져와서 상태에 설정
+        const savedImageUrl = localStorage.getItem('storageImg');
+        if (savedImageUrl) {
+            setImageUrl(savedImageUrl);
+        }
+
+        // fetchProfile 함수를 useEffect 내부에서 정의하고 호출
+        const fetchProfile = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/my/profile/${userId}`);
+                const userData = response.data[0]; // n번째 데이터
+                setProfileData(userData);
+                // console.log(userData)
+            } catch (error) {
+                console.error('Error: fetching profile data:', error);
+            }
+        };
+
+        // fetchProfile 함수 호출
+        fetchProfile();
+    }, [userId]); // userId가 변경될 때마다 호출
+
+    return (
+        <div className="profile-form inner" >
+            <div className="my-form__title">
+                <p className="my-form__text">나의 프로필</p>
+            </div>
+
+            <div className="profile-form__wrapper">
+                <div className="profile-form__img">
+                    {/* 업로드 된 이미지가 없다면 user_img폴더의 이미지를 표시 */}
+                    {imageUrl ? (<img src={imageUrl} alt="userimg" />) 
+                    : (<img className="profile-form__img" src="/user_img/basic.png" alt="IMG" />)}
+                    </div>
+                    <div className="profile-wrapper__btn">
+                    <button className="profile__btn" onClick={handleImgPopup}>사진 변경</button>
+                </div>
+
+                <div className="profile-content">
+                    {/* 이미지 팝업 */}
+                    {/* ImagePopup에 userId 전달 */}
+                    {showPopup && <ImagePopup userId={userId} onClose={() => setShowPopup(false)} handleProfileImg={handleProfileImg} />}
+
+                    <div className="profile-content-wrapper">
+                        {/* id식별 조건부 렌더링 */}
+                        {profileData.usertype === '2' || profileData.usertype === '3'
+                            ? (<p>사업자번호 {profileData.businessnumber}</p>) : null}
+
+                        <div className="profile-name-wrapper">
+                            <span className="profile__name">{profileData.username}</span>
+                            <span> 님</span>
+                        </div>
+                        <table class="profile-content__detail">
+                            <tr>
+                                <td>이메일</td>
+                                <td>{profileData.email}</td>
+                            </tr>
+                            <tr>
+                                <td>휴대전화</td>
+                                <td>{profileData.phonenumber}</td>
+                            </tr>
+                            <tr>
+                                <td>주소</td>
+                                <td>{profileData.address}</td>
+                            </tr>
+                            <tr>
+                                <td>상세주소</td>
+                                <td>{profileData.detailedaddress}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default ProfileForm;
