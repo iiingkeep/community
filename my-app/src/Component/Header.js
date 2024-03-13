f62a3365e63836fc0ac1e154889847b24ab446d9
HUJO733 <wngh1716@gmail.com>
2024-03-13 09:23:24 +0900

    메인, 탄소중립 페이지 이동시 스크롤 문제 해결

--- my-app/src/Component/Header.js
+++ my-app/src/Component/Header.js
@@ -1,34 +1,79 @@
-import React from 'react';
-import './Header.css'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
-const Header = ({loggedIn, handleLogout}) => {
+const Header = ({ loggedIn, handleLogout }) => {
   const navigate = useNavigate();
+
+  const handleMain = () => {
+    navigate("/");
+    window.scrollTo(0, 0);
+  };
+
+    const handleNetZero = () => {
+      navigate("/NetZero");
+      window.scrollTo(0, 0);
+    };
+
   return (
-    <div className='header'>
-      <div className='header__logo'>
-        <button className='header__logo--button' onClick={() => navigate("/")}>빵끗😊</button>
+    <div className="header">
+      <div className="header__logo">
+        <button className="header__logo--button" onClick={handleMain}>
+          빵끗😊
+        </button>
       </div>
-      <div className='header__menu'>
-        <button className='header__menu--button' onClick={() => navigate("/NetZero")}>탄소중립이란? </button>
-        <button className='header__menu--button' onClick={() => navigate("/news")}>환경이슈 </button>
-        <button className='header__menu--button' onClick={() => navigate("/Community")}>커뮤니티 </button>
-            </div>
-            <div className='header__button'>
-            {loggedIn ? (
-            <div>
-              <button className='header__button--button button' onClick={() => navigate("/MyPage")}>마이페이지</button>
-              <button className='header__button--button button' onClick={handleLogout}>로그아웃</button>
-            </div>
-          ) : (
-            <div>
-              <button className='header__button--button button' onClick={() => navigate("/Login")}>로그인</button>
-              <button className='header__button--button button' onClick={() => navigate("/RegisterPersonal")}>회원가입</button>
-            </div>
-          )}</div>
+      <div className="header__menu">
+        <button className="header__menu--button" onClick={handleNetZero}>
+          탄소중립이란?{" "}
+        </button>
+        <button
+          className="header__menu--button"
+          onClick={() => navigate("/news")}
+        >
+          환경이슈{" "}
+        </button>
+        <button
+          className="header__menu--button"
+          onClick={() => navigate("/Community")}
+        >
+          커뮤니티{" "}
+        </button>
+      </div>
+      <div className="header__button">
+        {loggedIn ? (
+          <div>
+            <button
+              className="header__button--button button"
+              onClick={() => navigate("/MyPage")}
+            >
+              마이페이지
+            </button>
+            <button
+              className="header__button--button button"
+              onClick={handleLogout}
+            >
+              로그아웃
+            </button>
           </div>
-          
-  )
-}
+        ) : (
+          <div>
+            <button
+              className="header__button--button button"
+              onClick={() => navigate("/Login")}
+            >
+              로그인
+            </button>
+            <button
+              className="header__button--button button"
+              onClick={() => navigate("/RegisterPersonal")}
+            >
+              회원가입
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
