12bd1c8c4869487bcfd98511d835e5c932fcbadb
smlsk12 <smlsk12@naver.com>
2024-03-17 15:36:16 +0900

    프로필폼 코드정리

--- my-app/src/Component/ProfileForm.js
+++ my-app/src/Component/ProfileForm.js
@@ -1,3 +1,105 @@
+// ProfileForm.js
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ImagePopup from "./ImagePopup";
+import "../Styles/MyPage.css";
+import { Icon } from '@iconify/react';
+
+
+const ProfileForm = ({ userId }) => {
+    // 회원 정보 상태관리
+    const [profileData, setProfileData] = useState([]);
+    // 이미지 URL 상태관리
+    const [imageUrl, setImageUrl] = useState("");
+    // 이미지팝업창 클릭이벤트 상태관리
+    const [showPopup, setShowPopup] = useState(false);
+
+
+
+    // // 이미지 업데이트 핸들러
+    const handleProfileImg = (url) => {
+        setImageUrl(url);
+    };
+    // 이미지팝업창 클릭이벤트
+    const handleImgPopup = () => {
+        setShowPopup(true);
+    };
+
+    useEffect(() => {
+        // 페이지 로드 시 저장된 이미지url 가져와서 상태에 설정
+        const savedImageUrl = localStorage.getItem('storageImg');
+        if (savedImageUrl) {
+            setImageUrl(savedImageUrl);
+        }
+
+        // fetchProfile 함수를 useEffect 내부에서 정의하고 호출
+        const fetchProfile = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/my/profile/${userId}`);
+                const userData = response.data[0]; // n번째 데이터
+                setProfileData(userData);
+                // console.log(userData)
+            } catch (error) {
+                console.error('Error: fetching profile data:', error);
+            }
+        };
+
+        // fetchProfile 함수 호출
+        fetchProfile();
+    }, [userId]); // userId가 변경될 때마다 호출
+
+    return (
+        <div className="profile-form" >
+            <div className="my-form__title">
+                <p className="my-form__text">나의 프로필</p>
+            </div>
+
+            <div className="profile-form__wrapper">
+                <div className="profile-form__img">
+                    {/* 업로드 된 이미지가 없다면 user_img폴더의 이미지를 표시 */}
+                    {imageUrl ? (<img src={imageUrl} alt="userimg" />)
+                        : (<img className="profile-form__img" src="/user_img/basic.png" alt="IMG" />)}
+                    <button className="profile__btn" onClick={handleImgPopup}><Icon className="profile__btn__icon" icon="mdi:photo-camera" /></button>
+                </div>
+            </div>
+
+            {/* 이미지 팝업 */} {/* ImagePopup에 userId 전달 */}
+            {showPopup && <ImagePopup userId={userId} onClose={() => setShowPopup(false)} handleProfileImg={handleProfileImg} />}
+
+            {/* id식별 조건부 렌더링 */}
+            {profileData.usertype === '2' || profileData.usertype === '3'
+                ? (<p>사업자번호 {profileData.businessnumber}</p>) : null}
+            <div className="profile-name-wrapper">
+                <span className="profile__username">{profileData.username}</span>
+                <span className="profile__sir">님</span>
+            </div>
+
+            <div class="profile-content__detail">
+                <table>
+                    <tr>
+                        <td>아이디</td>
+                        <td>{profileData.email}</td>
+                    </tr>
+                    <tr>
+                        <td>휴대전화</td>
+                        <td>{profileData.phonenumber && 
+                            profileData.phonenumber.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3')}</td>
+                    </tr>
+                    <tr>
+                        <td>주소</td>
+                        <td>{profileData.address}</td>
+                    </tr>
+                    <tr>
+                        <td>상세주소</td>
+                        <td>{profileData.detailedaddress}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
+};
+export default ProfileForm;
+
 // // ProfileForm.js
 // import axios from "axios";
 // import { useEffect, useState } from "react";
@@ -105,105 +207,3 @@
 //     );
 // };
 // export default ProfileForm;
-
-// ProfileForm.js
-import axios from "axios";
-import { useEffect, useState } from "react";
-import ImagePopup from "./ImagePopup";
-import "../Styles/MyPage.css";
-import { Icon } from '@iconify/react';
-
-
-const ProfileForm = ({ userId }) => {
-    // 회원 정보 상태관리
-    const [profileData, setProfileData] = useState([]);
-    // 이미지 URL 상태관리
-    const [imageUrl, setImageUrl] = useState("");
-    // 이미지팝업창 클릭이벤트 상태관리
-    const [showPopup, setShowPopup] = useState(false);
-
-
-
-    // // 이미지 업데이트 핸들러
-    const handleProfileImg = (url) => {
-        setImageUrl(url);
-    };
-    // 이미지팝업창 클릭이벤트
-    const handleImgPopup = () => {
-        setShowPopup(true);
-    };
-
-    useEffect(() => {
-        // 페이지 로드 시 저장된 이미지url 가져와서 상태에 설정
-        const savedImageUrl = localStorage.getItem('storageImg');
-        if (savedImageUrl) {
-            setImageUrl(savedImageUrl);
-        }
-
-        // fetchProfile 함수를 useEffect 내부에서 정의하고 호출
-        const fetchProfile = async () => {
-            try {
-                const response = await axios.get(`http://localhost:8000/my/profile/${userId}`);
-                const userData = response.data[0]; // n번째 데이터
-                setProfileData(userData);
-                // console.log(userData)
-            } catch (error) {
-                console.error('Error: fetching profile data:', error);
-            }
-        };
-
-        // fetchProfile 함수 호출
-        fetchProfile();
-    }, [userId]); // userId가 변경될 때마다 호출
-
-    return (
-        <div className="profile-form" >
-            <div className="my-form__title">
-                <p className="my-form__text">나의 프로필</p>
-            </div>
-
-            <div className="profile-form__wrapper">
-                <div className="profile-form__img">
-                    {/* 업로드 된 이미지가 없다면 user_img폴더의 이미지를 표시 */}
-                    {imageUrl ? (<img src={imageUrl} alt="userimg" />)
-                        : (<img className="profile-form__img" src="/user_img/basic.png" alt="IMG" />)}
-                    <button className="profile__btn" onClick={handleImgPopup}><Icon className="profile__btn__icon" icon="mdi:photo-camera" /></button>
-                </div>
-            </div>
-
-            {/* 이미지 팝업 */} {/* ImagePopup에 userId 전달 */}
-            {showPopup && <ImagePopup userId={userId} onClose={() => setShowPopup(false)} handleProfileImg={handleProfileImg} />}
-
-            {/* id식별 조건부 렌더링 */}
-            {profileData.usertype === '2' || profileData.usertype === '3'
-                ? (<p>사업자번호 {profileData.businessnumber}</p>) : null}
-            <div className="profile-name-wrapper">
-                <span className="profile__username">{profileData.username}</span>
-                <span className="profile__sir">님</span>
-            </div>
-
-            <div class="profile-content__detail">
-                <table>
-                    <tr>
-                        <td>아이디</td>
-                        <td>{profileData.email}</td>
-                    </tr>
-                    <tr>
-                        <td>휴대전화</td>
-                        <td>{profileData.phonenumber && 
-                            profileData.phonenumber.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3')}</td>
-                    </tr>
-                    <tr>
-                        <td>주소</td>
-                        <td>{profileData.address}</td>
-                    </tr>
-                    <tr>
-                        <td>상세주소</td>
-                        <td>{profileData.detailedaddress}</td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    );
-};
-export default ProfileForm;
